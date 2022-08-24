b745a3e3b0eed71579a2c1fd1272d3626ba98654
saurabh5155 <s.a.patel5155@gmail.com>
2022-08-24 13:55:36 +0530

    login done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { SessionService } from '../session.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  email:string=""
+  password:string=""
 
-  ngOnInit(): void {
+  constructor(private sessionService:SessionService,private router:Router,private toster:ToastrService) { }
+
+  login(){
+
+    console.log(this.email);
+    console.log(this.password);
+    
+    let loginCredentials={
+      "email":this.email,
+      "password":this.password
+    }
+
+    this.sessionService.loginApi(loginCredentials).subscribe(res=>{
+      this.toster.success("Login Successfull","",{ timeOut:1500 })
+      this.router.navigateByUrl("/home")
+    },err=>{
+      this.toster.error("Invalid loginCredentials","",{ timeOut:1500 })
+      this.router.navigateByUrl("/login")     
+    })
+
   }
 
 }
